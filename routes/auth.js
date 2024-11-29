a4f59117ca0ca9df6ee3198d6117a5b5758f06f3
Ramos Hassan <ramoshassan@Ramoss-MacBook-Pro.local>
2024-11-29 10:34:57 +0300

    fixed auth

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const auth = require('../middleware/auth'); // Make sure you have this middleware
+
 
 router.post('/register', async (req, res) => {
   try {
